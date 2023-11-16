645e329897d853780b7ba8113f474674caf2d486
zagros100 <hadi_k64f@yahoo.com>
2023-11-15 23:06:35 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -162,7 +162,12 @@ class TestAccountService(TestCase):
         """It should Update an Existing Account"""
         account = self._create_accounts(1)[0]
         print(account)
-        new_data = {"name": "Updated Name", "email": "updated@email.com", "address": "updated address", "phone_number": "updated phone num"}
+        new_data = {
+            "name": "Updated Name",
+            "email": "updated@email.com",
+            "address": "updated address",
+            "phone_number": "updated phone num"
+            }
         response = self.client.put(
             f"{BASE_URL}/{account.id}",
             json=new_data,
@@ -194,7 +199,7 @@ class TestAccountService(TestCase):
         """It should not allow an illegal method call"""
         resp = self.client.delete(BASE_URL)
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-    
+
     def test_security_headers(self):
         """It should return security headers"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
@@ -208,7 +213,7 @@ class TestAccountService(TestCase):
         }
         for key, value in headers.items():
             self.assertEqual(response.headers.get(key), value)
-    
+
     def test_cors_security(self):
         """It should return a CORS header"""
         response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
